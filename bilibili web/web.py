56d3772e4db17097cda4add132832367843419ee
huangbot-maker <1323287564@qq.com>
2023-07-30 11:13:18 +0800

    update

--- bilibili web/web.py
+++ bilibili web/web.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 if __name__ == '__main__':
     app = create_app()
     #CORS(app, resoures = r'/*')
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
